778408bca1ebed83b9849c9607072431d03cf904
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2022-10-04 14:45:39 +0300

    Adding some exercises' solutions in chapter one.

---
+++ Chapter_1/Exercise_1_11.java
@@ -0,0 +1,22 @@
+// Chapter 1 - Exercise 1.1.11
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_1_11
+{
+	public static void main( String[] args )
+	{
+		boolean[][] twoDimensionalBooleanArray = new boolean[ 5 ][ 5 ];
+		
+		for( int i = 0 ; i < twoDimensionalBooleanArray.length ; i++ )
+		{
+			for( int j = 0 ; j < twoDimensionalBooleanArray[ i ].length ; j++ )
+			{
+				twoDimensionalBooleanArray[ i ][ j ] = ( Math.random() ) > 0.5 ? true : false;
+				StdOut.print( twoDimensionalBooleanArray[ i ][ j ] );
+			}
+			StdOut.println();
+		}
+	}
+}
---
+++ Chapter_1/Exercise_1_5.java
@@ -0,0 +1,21 @@
+// Chapter 1 - Exercise 1.1.5
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_1_5
+{
+	public static void main( String[] args )
+	{
+		StdOut.println( "Enter two numbers to determine if they are between 0 and 1 or not: " );
+		double n1 = StdIn.readDouble();
+		double n2 = StdIn.readDouble();
+		
+		boolean isBothBetweenZeroAndOne = ( n1 > 0 && n1 < 1) && ( n2 > 0 && n2 < 1);
+		
+		if( isBothBetweenZeroAndOne )
+			StdOut.println( isBothBetweenZeroAndOne );
+		else
+			StdOut.println( isBothBetweenZeroAndOne );
+	}
+}
