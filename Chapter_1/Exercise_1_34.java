9574b5e3d8f67e8b25214acd648b787aac95572b
ErkamUzuncayir_Home <erkamuzuncayir@gmail.com>
2022-10-24 21:02:38 +0300

    Add: Exercises_1_33 and Exercises_1_34's solutions

---
+++ Chapter_1/Exercise_1_34.java
@@ -0,0 +1,10 @@
+// Chapter 1 - Exercise 1.1.34
+
+Print the maximum and minimum numbers -> Could be implemented as a filter
+Print the median of the numbers -> Requires saving all values
+Print the Kth smallest value, for K less than 100 -> Could be implemented as a filter with an array of size K
+Print the sum of the squares of the numbers -> Could be implemented as a filter
+Print the average of the N numbers -> Could be implemented as a filter
+Print the percentage of numbers greater than the average -> Requires saving all values
+Print the N numbers in increasing order -> Requires saving all values
+Print the N numbers in random order -> Requires saving all values
